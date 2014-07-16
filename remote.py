c86b9cf89a76c0b1175580d830f9412a23541ddc
remote <lange.richard.d@gmail.com>
2014-07-16 14:43:23 -0400

    fixed remote.py to get it running on remote ubuntu account

--- remote.py
+++ remote.py
@@ -11,17 +11,18 @@ if __name__ == '__main__':
     game = LetterLogic(LetterBoard(lines[0]))
     print "created game"
     # next 5 lines are current state of grid
-    grid = [[]]*5
+    grid = [None]*5
     if len(lines) > 5:
         for i in range(5):
             strvals = lines[i+1].split() # splits on all whitespace
             nums = [int(val) for val in strvals]
             grid[i] = nums
+            print nums
     # remaining lines are played words
     played_words = []
     for i in range(6, len(lines)):
         played_words.extend(lines[i].split())
-    
+    print grid,played_words
     game.initialize(grid, played_words)
     
     # run analytics for best words 1-3
